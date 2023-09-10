a30e58bd2cb8679dd45f18561df263ff5518ab6c
CloudGraficaWeb <113855758+cloudgraficaweb@users.noreply.github.com>
2023-09-09 21:54:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("downloadButton").addEventListener("click", function() {
+    // Substitua a URL_DO_ARQUIVO_AQUI pela URL do arquivo que você deseja disponibilizar para download
+    window.location.href = "URL_DO_ARQUIVO_AQUI";
+});
