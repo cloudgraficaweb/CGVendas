f99a8f226fe96d27a884a5cf172ae8ef584f6389
CloudGraficaWeb <113855758+cloudgraficaweb@users.noreply.github.com>
2024-01-30 15:24:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
 // script.js
-document.getElementById("downloadButton").addEventListener("click", function() {
-    // Substitua a URL_DO_ARQUIVO_AQUI pela URL do arquivo que você deseja disponibilizar para download
-    window.location.href = "URL_DO_ARQUIVO_AQUI";
-});
