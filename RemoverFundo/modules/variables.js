cf5b124b0749192454adf64c9295fd7c8464ab9b
CloudGraficaWeb <113855758+cloudgraficaweb@users.noreply.github.com>
2024-09-12 12:35:44 -0300

    Delete RemoverFundo directory

--- RemoverFundo/modules/variables.js
+++
@@ -1,3 +0,0 @@
-export const Upload = document.getElementById('upload-btn');
-export const Submit = document.getElementById('remove-btn');
-export const Download = document.getElementById('download-btn');
